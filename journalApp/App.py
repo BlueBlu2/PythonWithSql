49be5113d5ba2c607d5ed940c9ccaef70f0aee4d
BlueBlu2 <eng.abdalrhmn.mohamed@gmail.com>
2025-11-03 21:18:32 +0200

    Sqlite add with Add and Get Rows

--- journalApp/App.py
+++ journalApp/App.py
@@ -1,4 +1,4 @@
-from database import add_entry, get_entries
+from database import add_entry, get_entries, create_table, commit_changes, close_connection
 
 menu = """Please select one of the following options:
 1) Add new entry for today.
@@ -10,15 +10,20 @@ Your selection:
 
 welcome = "Welcome to the programming diary!"
 
+create_table()
+commit_changes()
+
 def prompt_new_entry():
     entry_content = input("What news you have today? ")
     entry_date = input("Enter the date: ")
     
     add_entry(entry_content, entry_date)
 
+
 def view_entries(entries):
     for entry in entries:
-        print(f"{entry["date"]}\n{entry['content']}\n\n")
+        print(f"\n\n{entry["data"]}\n{entry["content"]}\n\n")
+
 
 print(welcome)
 
--- journalApp/database.py
+++ journalApp/database.py
@@ -1,9 +1,22 @@
-entries = []
+import sqlite3
+
+_connection = sqlite3.connect("data.db")
+_connection.row_factory = sqlite3.Row
+
+def create_table():
+    _connection.execute("CREATE TABLE IF NOT EXISTS entries(content TEXT, data TEXT)")
+    
+def commit_changes():
+    _connection.commit()
+    
+def close_connection():
+    _connection.close()
 
 def add_entry(content, date):
-
-    entries.append({"content":content,'date':date})
+    with _connection:
+        _connection.execute("INSERT INTO entries VALUES (?,?)", (content, date))
     
     
 def get_entries():
-    return entries
+    cursor = _connection.execute("SELECT * FROM entries")
+    return cursor
